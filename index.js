961266ee798ad75c6b3c9ead5816293093f5a802
Adrian Chen <adrian.evangelion@gmail.com>
2019-01-17 14:50:12 +0000

    EDIT - hooks to handle bad swipe

--- index.js
+++ index.js
@@ -18,34 +18,51 @@ stdin.on( 'data', function( key ){
   // ctrl-c ( end of text )
   console.log('swipeData');
   console.log(swipeData);
-  if ( key === '?' ) {
+  if ( key === '?' ) { //end of the swipe
     // const employeeID = swipeData.substring(0, 6);
 
     // console.log(employeeID + ' has swiped at ' + new Date() + '!');
+    swipeData = swipeData.match(/[0-9]/g).join('');
+    // console.log('hereree');
+    console.log('End of swipe !!!!!!')
+    console.log(swipeData)
+    console.log(swipeData.length)
+    console.log('!!!!!!!!!!!!!!!!!!!')
 
-    const fs = require('fs');
 
-    let db = new sqlite3.Database('./swipe_db.db', (err) => {
-      if (err) {
-        return console.error(err.message);
-      }
+    if (swipeData.length !== 12) {
+      console.log('🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨');
+      console.log('Theres a swipe Error!');
+      console.log(swipeData)
+      console.log('🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨');
+      swipeData = ''
+    } else {
+      const fs = require('fs');
 
-      console.log('Connected to the in-memory SQlite database.');
-
-      db.run(`INSERT INTO swipes(employee_id,datetime) VALUES(?,?)`, [swipeData,new Date()], function(err) {
-        db.close();
+      let db = new sqlite3.Database('./swipe_db.db', (err) => {
         if (err) {
-          return console.log(err.message);
+          return console.error(err.message);
         }
-      })
-    });
+        console.log('Connected to the in-memory SQlite database.');
+        console.log('swipeData inside db write');
+        console.log(swipeData);
 
-    fs.appendFile('./swipes.txt', `${swipeData},${new Date()}\n`, function (err) {
-      if (err) throw err;
-      swipeData = '';
-      console.log('Saved!');
-    });
-    // process.exit();
+        db.run(`INSERT INTO swipes(employee_id,datetime) VALUES(?,?)`, [swipeData,new Date()], function(err) {
+          db.close();
+          if (err) {
+            return console.log(err.message);
+          }
+        })
+      });
+
+      fs.appendFile('./swipes.txt', `${swipeData},${new Date()}\n`, function (err) {
+        if (err) throw err;
+        swipeData = '';
+        console.log('Saved!');
+      });
+      // process.exit();
+    }
+
   }
 
   if ( key === '|' ) {
@@ -55,5 +72,8 @@ stdin.on( 'data', function( key ){
   // write the key to stdout all normal like
   // console.log('key being pressed:');
   // process.stdout.write( key );
-  swipeData = swipeData + key;
+  if (key !== '?') {
+    swipeData = swipeData + key;
+  }
+
 });
