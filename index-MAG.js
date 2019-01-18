a364ed24b31763007ee9d0940168774e51bd8e54
Adrian Chen <adrian.evangelion@gmail.com>
2019-01-18 14:29:29 +0000

    Fixing hid stream

--- index-MAG.js
+++ index-MAG.js
@@ -1,4 +1,4 @@
-const HidStream = require('node-hid-stream')
+const HidStream = require('node-hid-stream').HidStream
 var stdin = process.stdin;
 const sqlite3 = require('sqlite3').verbose();
 request = require('request-json');
