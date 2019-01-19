68e4207720356c120ec24a2840470398c62044a0
Pi <pi@pi.com>
2019-01-19 11:59:24 +0000

    Brought in pi-rc522 to use interrupts.

---
+++ new-read.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from pirc522 import RFID
+import requests
+import datetime
+import time
+import signal
+import sys
+import array
+
+def handle_sigint(sig, frame):
+    exit(0)
+
+signal.signal(signal.SIGINT, handle_sigint)
+
+rdr = RFID()
+
+epoch = datetime.datetime.utcfromtimestamp(0)
+
+def unix_time_millis(dt):
+    return (dt - epoch).total_seconds() * 1000.0
+
+# Taken from https://github.com/pimylifeup/MFRC522-python
+def uid_to_num(uid):
+    n = 0
+    for i in uid:
+        n = n * 256 + i
+    return n
+
+last_id_seen = ''
+last_time_seen = 0
+
+try:
+    while True:
+       rdr.wait_for_tag()
+       (error, tag_type) = rdr.request()
+       if not error:
+           (error, id) = rdr.anticoll()
+           if not error:
+                id = uid_to_num(id)
+                dte = unix_time_millis(datetime.datetime.now())
+    
+                if last_id_seen == id and last_time_seen < (dte - 10000):
+                    continue
+    
+                last_time_seen = dte
+                json = {"employee_id":str(id)}
+                headers = {'Content-type':'application/json'}
+                print("Sending POST request.")
+                print(json)
+                r = requests.post('http://localhost/swipes', json=json, headers=headers)
+                print("HTTP Response code: " + str(r.status_code))
+            
+                last_id_seen = id
+            
+                print("status_code: {}".format(r.status_code))
+
+finally:
+    rdr.cleanup()
+
+# We don't need the auth and sector read code right now
+#            # Select Tag is required before Auth
+#            if not rdr.select_tag(id):
+#                # Auth for block 10 (block 2 of sector 2) using default shipping key A
+#                if not rdr.card_auth(rdr.auth_a, 10, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], id):
+#                    # This will print something like (False, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+#                    # Always stop crypto1 when done working
+#                    rdr.stop_crypto()
+
